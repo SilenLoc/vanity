3282f28498b724c34dc5559202964853dc83957d
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2022-11-21 13:12:31 +0100

    first commit, testing out slfs and logback

---
+++ app/src/main/kotlin/easyreading/app/Init.kt
@@ -0,0 +1,15 @@
+package easyreading.app
+
+import easyreading.utilities.Log
+
+class Init {
+
+
+  init {
+    Log.info { "init App" }
+  }
+
+  fun files() = Unit
+
+
+}
---
+++ utilities/src/main/kotlin/easyreading/utilities/Log.kt
@@ -0,0 +1,13 @@
+package easyreading.utilities
+
+import mu.KotlinLogging
+
+
+object Log {
+
+  private val logger = KotlinLogging.logger(Thread.currentThread().stackTrace[2].className)
+
+  fun info(msg: () -> Any?) = logger.info(msg().toString())
+
+
+}
